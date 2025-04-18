9f7b1b382ddaa9fa1adea9436897c26363777ad2
I'm DK <mcst014014@gmail.com>
2025-04-18 02:46:11 -0300

    Emplementando js e estilizando comentário

---
+++ content.js
@@ -0,0 +1,146 @@
+window.content = {
+    pt: {
+        navbar: {
+            about: "Sobre",
+            projects: "Projetos",
+            languages: "Linguagens Dev",
+            blog: "Blog",
+            contact: "Contato",
+            hire: "Contrate-me",
+        },
+        cover: {
+            title: "Matheus Rodrigues",
+            subtitle: "Desenvolvedor Profissional Web",
+            myWork: "Meu Trabalho",
+            download: "Baixar CV",
+        },
+        projects: [
+            {
+                icon: "assets/T-Shirt.svg",
+                title: "DK's Looks",
+                description: "O estilo que acompanha seu ritmo. Vista-se com atitude. Vista-se com DK’s Looks!!!"
+            },
+            {
+                icon: "assets/OneTap.svg",
+                title: "One Tap",
+                description: "OneTap - Com tecnologia e criatividade, conectamos pessoas através do som!"
+            },
+            {
+                icon: "assets/One Piece.svg",
+                title: "One Piece",
+                description: "Luffy e sua tripulação em busca do tesouro One Piece. Frutas do Diabo, Haki, batalhas inesquecíveis e muito mais."
+            },
+            {
+                icon: "assets/StartZ.svg",
+                title: "StartZ",
+                description: "StartZ - Sua plataforma de lançamento para ideias inovadoras. Comece agora e molde o futuro!"
+            },
+            {
+                icon: "assets/ParTour.svg",
+                title: "PartTour",
+                description: "PartTour - Nosso objetivo é levar a paixão pela música a cada nota!"
+            },
+            {
+                icon: "assets/Dream Coffee.svg",
+                title: "Dream Coffee",
+                description: "Dream Coffee – Um café, infinitas sensações! Cada xícara conta uma história de aroma e sabor."
+            }
+        ]
+    },
+    en: {
+        navbar: {
+            about: "About",
+            projects: "Projects",
+            languages: "Dev Languages",
+            blog: "Blog",
+            contact: "Contact",
+            hire: "Hire me",
+        },
+        cover: {
+            title: "Matheus Rodrigues",
+            subtitle: "Professional Web Developer",
+            myWork: "My Work",
+            download: "Download CV",
+        },
+        projects: [
+            {
+                icon: "assets/T-Shirt.svg",
+                title: "DK's Looks",
+                description: "The style that keeps up with your rhythm. Dress with attitude. Dress with DK’s Looks!!!"
+            },
+            {
+                icon: "assets/OneTap.svg",
+                title: "One Tap",
+                description: "OneTap - With technology and creativity, we connect people through sound!"
+            },
+            {
+                icon: "assets/One Piece.svg",
+                title: "One Piece",
+                description: "Luffy and his crew in search of the One Piece treasure. Devil Fruit and Haki, unforgettable battles and much more."
+            },
+            {
+                icon: "assets/StartZ.svg",
+                title: "StartZ",
+                description: "StartZ - Your launchpad for innovative business ideas. Start now and shape the future!"
+            },
+            {
+                icon: "assets/ParTour.svg",
+                title: "PartTour",
+                description: "PartTour - Our goal is to bring passion for music to every note!"
+            },
+            {
+                icon: "assets/Dream Coffee.svg",
+                title: "Dream Coffee",
+                description: "Dream Coffee – One coffee, infinite sensations! Each cup tells a story of aroma and flavor."
+            }
+        ]
+    },
+    es: {
+        navbar: {
+            about: "Sobre mí",
+            projects: "Proyectos",
+            languages: "Lenguajes Dev",
+            blog: "Blog",
+            contact: "Contacto",
+            hire: "Contrátame",
+        },
+        cover: {
+            title: "Matheus Rodrigues",
+            subtitle: "Desarrollador Web Profesional",
+            myWork: "Mi Trabajo",
+            download: "Descargar CV",
+        },
+        projects: [
+            {
+                icon: "assets/T-Shirt.svg",
+                title: "DK's Looks",
+                description: "El estilo que sigue tu ritmo. ¡Vístete con actitud, vístete con DK’s Looks!"
+            },
+            {
+                icon: "assets/OneTap.svg",
+                title: "One Tap",
+                description: "OneTap - ¡Con tecnología y creatividad, conectamos personas a través del sonido!"
+            },
+            {
+                icon: "assets/One Piece.svg",
+                title: "One Piece",
+                description: "Luffy y su tripulación en busca del tesoro One Piece. Frutas del Diablo, Haki, batallas inolvidables y mucho más."
+            },
+            {
+                icon: "assets/StartZ.svg",
+                title: "StartZ",
+                description: "StartZ - Tu plataforma de lanzamiento para ideas innovadoras. ¡Empieza ahora y moldea el futuro!"
+            },
+            {
+                icon: "assets/ParTour.svg",
+                title: "PartTour",
+                description: "PartTour - ¡Nuestro objetivo es llevar la pasión por la música a cada nota!"
+            },
+            {
+                icon: "assets/Dream Coffee.svg",
+                title: "Dream Coffee",
+                description: "Dream Coffee – ¡Un café, infinitas sensaciones! Cada taza cuenta una historia de aroma y sabor."
+            }
+        ]
+    }
+};
